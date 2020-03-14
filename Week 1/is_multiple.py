76d7197029b244baaddff34b4f795e2ed779c0d2
Tye <tye.hickman89@gmail.com>
2020-03-14 01:16:34 -0500

    Week 1 code

---
+++ Week 1/is_multiple.py
@@ -0,0 +1,8 @@
+#Tye Hickman
+# Week 1
+# Is multiple: AKA modulo to the rescue
+def is_multiple(n,m):
+    if n % m == 0:
+        return True
+    else:
+        return False
---
+++ Week 1/punishment.py
@@ -0,0 +1,8 @@
+#Tye Hickman
+# Week 1
+# Reinforcement and Punishment
+punishString = "I will never spam my friends again."
+
+for i in range(100):
+    # print(str(i) + " " + punishString)
+    print(punishString)
