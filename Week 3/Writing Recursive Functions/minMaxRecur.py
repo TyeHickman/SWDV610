0c4e377de93744262572ea4457389a038384e535
thickman@vertafore.com <thickman@vertafore.com>
2020-03-28 21:36:36 -0500

    added supporting documentation for float('inf')

--- Week 3/Writing Recursive Functions/minMaxRecur.py
+++ Week 3/Writing Recursive Functions/minMaxRecur.py
@@ -2,6 +2,9 @@
 #Week 3
 #Recursion: Minimum and Maximum values of a sequence
 
+#Had to work out how to represent an unknown number
+#Luckily the built in type float() has some tricks we can use
+#https://docs.python.org/3.7/library/functions.html#float
 def findMinMax( aList, i=0, mini= float('inf'), maxi= float('-inf')):
     e=aList[i]
     #element less than minimum
