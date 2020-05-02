5952ac6248377d4a5e872558baaccfa832ae9c16
Tye <tye.hickman89@gmail.com>
2020-05-02 00:52:49 -0500

    WIP draw lines or arrows for visited squares. WIP TODOs

--- ScratchBoard/chessboard.py
+++ ScratchBoard/chessboard.py
@@ -1,5 +1,9 @@
 import graphics as g
 from graphics import *
+import Graph as gr
+from Graph import *
+import graphedKnightsTour as gkt
+import time
 
 WN_MULT = 100
 
@@ -15,6 +19,7 @@ def genSquares(boardSize):
     sqDict  = {}
     #This loop applies chr and ord to generate a letter of a column
     for i in range(boardSize):
+        # create column chars
         colChar = chr(ord('a') + i)
         x = i * WN_MULT
         x2 = x + WN_MULT
@@ -30,29 +35,74 @@ def genSquares(boardSize):
     return sqDict
 
 def main():
-    bSize = int(input("Board Size: "))
+    # bSize = int(input("Board Size: "))
+    bSize = 8
+    searchLimit = (bSize*bSize) - 1
     bWin = getBoard(bSize)
+    #Get graph of all legal moves
+    knightsGraph = gkt.knightGraph(bSize)
+    # for em in knightsGraph:
+    #     print(em.getId())
 
+
+    # initialize this for the tour
+    knightsPath = []
+
+    # TODO: figure out how to let user select starting square
+    # TODO: make '63' as limit in knightTour dynamic
+    knightsTour = gkt.knightTour(0,knightsPath,knightsGraph.getVertex(4),searchLimit)
+
+
+
+    # for vert in knightsTour:
+    #     print(vert.getId())
+
+    # TODO: uncomment to display chessboard
     # Now I have a nice dictionary with all my squares
+
+    vs = {}
     squares = genSquares(bSize)
+    i = 0
     for key in squares:
         # print(type(squares[key]))
+        vs[i] = squares[key]
+        label = Text(squares[key].getCenter(),str(key) + " " + str(i))
+        i = i + 1
+        
         squares[key].draw(bWin)
-    
+        label.draw(bWin)
 
     # Manually undraw, setfill, and redraw
     # TODO: make a method that does this for 'visited'
-    print(squares['a1'])
-    squares['a1'].undraw()
-    squares['a1'].setFill('red')
-    squares['a1'].draw(bWin)
+    for vert in knightsTour:
+        sqId = vert.getId()
+        if sqId in vs.keys():
+            # print(sqId)
+            # print(vs[sqId])
+            time.sleep(.4)
+            vs[sqId].undraw()
+            vs[sqId].setFill('brown')
+            vs[sqId].draw(bWin)
+
+            #Draw some lines
+            # if sqId > 0:
+            #     mL = Line(vs[sqId].getCenter(),vs[sqId].getCenter())
+            #     mL.setFill('Blue')
+            #     mL.draw(bWin)
+
+
+    # print(squares['a1'])
+    # squares['a1'].undraw()
+    # squares['a1'].setFill('red')
+    # squares['a1'].draw(bWin)
 
 
     # Line experiment
+    #  TODO: Method to draw line between vertices as they are traversed
+    # mL = Line(squares['a1'].getCenter(),squares['c8'].getCenter())
+    # mL.setFill('red')
+    # mL.draw(bWin)
 
-    mL = Line(squares['a1'].getCenter(),squares['c8'].getCenter())
-    mL.setFill('red')
-    mL.draw(bWin)
-
+    # TODO: Handle getmouse
     bWin.getMouse()
 main()
---
+++ ScratchBoard/graphedKnightsTour.py
@@ -0,0 +1,94 @@
+MOVE_OFFSETS = (
+              (-1, -2), ( 1, -2),
+    (-2, -1),                     ( 2, -1),
+    (-2,  1),                     ( 2,  1),
+              (-1,  2), ( 1,  2),
+)
+from Graph import *
+
+# Create a graph of legal moves.
+def knightGraph(bdSize):
+    ktGraph = Graph()
+    for row in range(bdSize):
+        for col in range(bdSize):
+            # print("("+str(row)+"*"+str(bdSize)+")+"+str(col)+"="+str(posToNodeId(col,row,bdSize)))
+            nodeId = posToNodeId(row,col,bdSize)
+            newPositions = genLegalMoves(row,col,bdSize)
+            for e in newPositions:
+                nid = posToNodeId(e[0],e[1],bdSize)
+                ktGraph.addEdge(nodeId,nid)
+    return ktGraph
+
+def posToNodeId(row, column, board_size):
+    return (row * board_size) + column
+
+def genLegalMoves(x,y,bdSize):
+    newMoves = []
+    # moveOffsets = [(-1,-2),(-1,2),(-2,-1),(-2,1),
+    #                ( 1,-2),( 1,2),( 2,-1),( 2,1)]
+    moveOffsets = MOVE_OFFSETS
+    for i in moveOffsets:
+        newX = x + i[0]
+        newY = y + i[1]
+        # Recursively check coordinate is on board
+        if legalCoord(newX,bdSize) and legalCoord(newY,bdSize):
+            newMoves.append((newX,newY))
+    return newMoves
+
+# Verify that the coordinate is on the board.
+def legalCoord(x,bdSize):
+    if x >= 0 and x < bdSize:
+        return True
+    else:
+        return False
+
+def knightTour(n,path,u,limit):
+        u.setColor('gray')
+        path.append(u)
+        if n < limit:
+            # nbrList = list(u.getConnections())
+            nbrList = orderByAvail(u)
+            i = 0
+            done = False
+            while i < len(nbrList) and not done:
+                if nbrList[i].getColor() == 'white':
+                    done = knightTour(n+1, path, nbrList[i], limit)
+                i = i + 1
+            if not done:  # prepare to backtrack
+                path.pop()
+                u.setColor('white')
+        else:
+            done = True
+        return path
+
+def orderByAvail(n):
+    resList = []
+    for v in n.getConnections():
+        if v.getColor() == 'white':
+            c = 0
+            for w in v.getConnections():
+                if w.getColor() == 'white':
+                    c = c + 1
+            resList.append((c,v))
+    resList.sort(key=lambda x: x[0])
+    return [y[1] for y in resList]
+
+
+
+
+# def main():
+#     # bSize = int(input("Board Size: "))
+#     bSize = 8
+
+#     # kt = find_solution_for(bSize, warnsdorffs_heuristic)
+#     kt = knightGraph(bSize)
+#     # for key in kt:
+#     #     print(key)
+#     kPath = []
+#     kTour = knightTour(0,kPath,kt.getVertex(0),63)
+#     for vert in kTour:
+#         print(vert.getId())
+#     # print(kTour)
+#     # for vert in  kt.getVertices():
+#     #     print(vert)
+# main()
